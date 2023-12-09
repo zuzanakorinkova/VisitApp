ae3b219b0f8a3a06e3ba78e8acd9c7fbb1129a0c
Zuzana <44017111+zuzanakorinkova@users.noreply.github.com>
2023-12-09 22:53:07 +0100

    backend configuration

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import Navigation from './src/navigation';
+import { Amplify } from 'aws-amplify';
+import config from './src/aws-exports';
+
+
+Amplify.configure(config);
 
 function App(): JSX.Element {
   return (
