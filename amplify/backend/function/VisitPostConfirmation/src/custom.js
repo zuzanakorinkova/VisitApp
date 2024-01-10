0de607bef06c7d2e66518584711cebf4384bcc16
Zuzana <44017111+zuzanakorinkova@users.noreply.github.com>
2024-01-10 17:12:41 +0100

    lambda functions

---
+++ amplify/backend/function/VisitPostConfirmation/src/custom.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event, context) => {
+  // insert code to be executed by your lambda trigger
+  console.log('Lambda function!!!');
+  console.log(event);
+  return event;
+};
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,11 +3,22 @@ export type AmplifyDependentResourcesAttributes = {
     "Visit": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "VisitPostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
