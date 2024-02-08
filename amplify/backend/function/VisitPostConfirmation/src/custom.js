0a9c778daecdc72a2134fd24ed8b0e92cfcff4e4
Zuzana <44017111+zuzanakorinkova@users.noreply.github.com>
2024-02-08 20:14:13 +0100

    lambda function users

--- amplify/backend/function/VisitPostConfirmation/src/custom.js
+++ amplify/backend/function/VisitPostConfirmation/src/custom.js
@@ -1,9 +1,77 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+const AWS = require('aws-sdk');
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+//process.env
+const env = process.env.ENV;
+const AppSyncID = process.env.API_VISIT_GRAPHQLAPIIDOUTPUT;
+
+const TableName = `User-${AppSyncID}-${env}`; //TableName-AppSync ID-env
+
+const userExists = async id => {
+  const params = {
+    TableName,
+    Key: id,
+  };
+  try {
+    const response = await docClient.get(params).promise();
+    return !!response?.item;
+  } catch (e) {
+    return false;
+  }
+};
+
+const saveUser = async user => {
+  const date = new Date();
+  const timestamp = date.toISOString();
+  const Item = {
+    ...user,
+    __typename: 'User',
+    createdAt: timestamp,
+    updatedAt: timestamp,
+  };
+  const params = {
+    TableName,
+    Item,
+  };
+  try {
+    await docClient.put(params).promise();
+  } catch (e) {
+    console.log(e);
+  }
+};
 exports.handler = async (event, context) => {
   // insert code to be executed by your lambda trigger
   console.log('Lambda function!!!');
   console.log(event);
+  if (!event?.request.userAttributes) {
+    console.log('No user data available');
+    return;
+  }
+
+  const {sub, name, email} = event.request.userAttributes;
+
+  const newUser = {
+    id: sub,
+    name: name,
+    email: email,
+    nofPosts: 0,
+    nofLikes: 0,
+  };
+
+  // check if the user already exists
+
+  // if not, save the user to db
+
+  if (!(await userExists(newUser.id))) {
+    await saveUser(newUser);
+    console.log(`User ${newUser.id} has been saved to the database`);
+  } else {
+    console.log(`User ${newUser.id} already exists`);
+  }
+
   return event;
 };
--- amplify/backend/function/VisitPostConfirmation/src/index.js
+++ amplify/backend/function/VisitPostConfirmation/src/index.js
@@ -1,4 +1,10 @@
-/**
+/* Amplify Params - DO NOT EDIT
+	API_VISIT_GRAPHQLAPIENDPOINTOUTPUT
+	API_VISIT_GRAPHQLAPIIDOUTPUT
+	API_VISIT_GRAPHQLAPIKEYOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT *//**
  * @fileoverview
  *
  * This CloudFormation Trigger creates a handler which awaits the other handlers
